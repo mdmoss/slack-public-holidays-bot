93b7a087015583ad7d43103d7bd1ae254ecbfb46
Matthew Moss <matthew@premonition.io>
2024-08-28 13:55:32 +1000

    Use nager.date API for public holidays

--- src/main.rs
+++ src/main.rs
@@ -4,18 +4,16 @@ use clap::Parser;
 use itertools::Itertools;
 use serde::Deserialize;
 use ureq::OrAnyStatus;
+use isocountry;
 
 use std::{env, time::Duration};
 
-// The Abstract API returns more than just public holidays - these are the ones we're sure we want to include.
-// Originally we filtered out disallowed types (assuming it was preferable to fail-open and include if we're
-// not sure) but it turns out there's too many types to reasonably manage.
-const ALLOWED_HOLIDAY_TYPES: &[&str] = &["National", "Local holiday"];
+// There's a list of allowed types at https://date.nager.at/Api.
+const ALLOWED_HOLIDAY_TYPES: &[&str] = &["Public", "Optional"];
 
 fn main() {
     let args: Args = Args::parse();
 
-    let abstract_api_key = require_from_env("ABSTRACT_API_KEY");
     let slack_webhook_url = require_from_env("SLACK_WEBHOOK_URL");
 
     let date = match args.date {
@@ -30,7 +28,7 @@ fn main() {
 
     let holidays: Vec<Holiday> = country_codes
         .flat_map(|cc| {
-            let from_api = fetch_holidays_from_abstract(&abstract_api_key, cc, date);
+            let from_api = fetch_holidays_from_nager(cc, date);
             match from_api {
                 Ok(results) => results,
                 Err(e) => {
@@ -58,56 +56,36 @@ struct Args {
     countries: String,
 }
 
-const ABSTRACT_HOLIDAYS_API_URL: &str = "https://holidays.abstractapi.com/v1/";
+const NAGER_HOLIDAYS_URL_BASE: &str = "https://date.nager.at/api/v3/PublicHolidays";
 
-fn fetch_holidays_from_abstract(
-    api_key: &str,
+fn fetch_holidays_from_nager(
     country: &str,
     date: NaiveDate,
 ) -> Result<Vec<Holiday>> {
-    let result = ureq::get(ABSTRACT_HOLIDAYS_API_URL)
-        .query("api_key", api_key)
-        .query("country", country)
-        .query("year", date.year_ce().1.to_string().as_str())
-        .query("month", (date.month0() + 1).to_string().as_str())
-        .query("day", (date.day0() + 1).to_string().as_str())
+    let year = date.year_ce().1;
+    let url = format!("{NAGER_HOLIDAYS_URL_BASE}/{year}/{country}");
+    let result = ureq::get(&url)
         .call()?
         .into_json::<Vec<Holiday>>()?
         .into_iter()
         .filter(|h| {
-            h.r#type
-                .as_ref()
-                .is_some_and(|t| ALLOWED_HOLIDAY_TYPES.iter().any(|tt| t == tt))
-        })
-        .map(|mut h| {
-            h.drop_empty_string_values();
-            h
+            h.date == date.to_string() &&
+            h.types.iter().any(|t| ALLOWED_HOLIDAY_TYPES.iter().any(|allowed_type| allowed_type == t))
         })
         .collect_vec();
-
-    // From what I've observed, the rate limit for the free tier of the API is 1 req/s.
-    // The helpful AI built in to their documentation couldn't confirm that, though.
-    // Sleeping for a second here is simple and should do the trick.
-    std::thread::sleep(Duration::from_secs(1));
 
     Ok(result)
 }
 
 #[derive(Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
 struct Holiday {
+    date: String,
     name: String,
-    name_local: Option<String>,
-    country: Option<String>,
-    location: Option<String>,
-    r#type: Option<String>,
-}
-
-impl Holiday {
-    fn drop_empty_string_values(&mut self) {
-        self.name_local = self.name_local.as_ref().filter(|v| !v.is_empty()).cloned();
-        self.country = self.country.as_ref().filter(|v| !v.is_empty()).cloned();
-        self.location = self.location.as_ref().filter(|v| !v.is_empty()).cloned();
-    }
+    local_name: String,
+    types: Vec<String>,
+    counties: Option<Vec<String>>,
+    country_code: String,
 }
 
 fn require_from_env(key: &str) -> String {
@@ -131,65 +109,65 @@ fn send_to_slack(webhook_url: &str, holidays: Vec<Holiday>) -> Result<()> {
         }
     ));
 
-    let binding = holidays.iter().into_group_map_by(|h| h.location.clone());
-    let mut holidays_by_location: Vec<(&Option<String>, &Vec<&Holiday>)> = binding.iter().collect();
+    let binding = holidays.iter().into_group_map_by(|h| h.country_code.clone());
+    let mut holidays_by_country: Vec<(&String, &Vec<&Holiday>)> = binding.iter().collect();
 
-    holidays_by_location.sort_by_key(|(location, _)| *location);
+    holidays_by_country.sort_by_key(|(location, _)| *location);
 
-    for (location, holidays) in holidays_by_location {
-        if let Some(location) = location {
-            message_blocks.push(ureq::json!(
-                {
-                    "type": "section",
-                    "text": {
-                        "type": "mrkdwn",
-                        "text": format!("_{}_", location),
-                    }
+    for (location, holidays) in holidays_by_country {
+
+        let country_name = isocountry::CountryCode::for_alpha2_caseless(&location).map(|c| c.name()).unwrap_or(location);
+
+        message_blocks.push(ureq::json!(
+            {
+                "type": "section",
+                "text": {
+                    "type": "mrkdwn",
+                    "text": format!("_{}_", country_name),
                 }
-            ));
+            }
+        ));
 
-            let holiday_lines: Vec<serde_json::Value> = holidays
-                .iter()
-                .map(|h| {
-                    let mut elements: Vec<serde_json::Value> = Vec::new();
+        let holiday_lines: Vec<serde_json::Value> = holidays
+            .iter()
+            .map(|h| {
+                let mut elements: Vec<serde_json::Value> = Vec::new();
 
-                    elements.push(ureq::json!({
-                        "type": "text",
-                        "text": h.name,
-                        "style": {
-                            "bold": true
-                        }
-                    }));
-
-                    if let Some(name_local) = &h.name_local {
-                        elements.push(ureq::json!({
-                            "type": "text",
-                            "text": format!(" ({})", name_local),
-                            "style": {
-                                "italic": true
-                            }
-                        }));
+                elements.push(ureq::json!({
+                    "type": "text",
+                    "text": h.local_name,
+                    "style": {
+                        "bold": true
                     }
+                }));
 
-                    ureq::json!({
-                        "type": "rich_text_section",
-                        "elements": elements,
-                    })
+                elements.push(ureq::json!({
+                    "type": "text",
+                    "text": format!(" ({})", h.name),
+                    "style": {
+                        "italic": true
+                    }
+                }));
+
+                ureq::json!({
+                    "type": "rich_text_section",
+                    "elements": elements,
                 })
-                .collect();
+            })
+            .collect();
 
-            message_blocks.push(ureq::json!(
-                {
-                    "type": "rich_text",
-                    "elements": [
-                        {
-                        "type": "rich_text_list",
-                        "style": "bullet",
-                        "elements": holiday_lines,
-                    }]
-                }
-            ))
-        }
+        message_blocks.push(ureq::json!(
+            {
+                "type": "rich_text",
+                "elements": [
+                    {
+                    "type": "rich_text_list",
+                    "style": "bullet",
+                    "elements": holiday_lines,
+                }]
+            }
+        ))
+
     }
 
     let message = ureq::json!({
